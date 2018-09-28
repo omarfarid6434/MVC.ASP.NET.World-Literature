ce060823d2eca6b2159f3c6c5fd878d15c8cd458
Frarid Hossain <omarfarid778@gmail.com>
2018-09-29 05:36:12 +0600

    Enable migration and Update the domain model

---
+++ MVC.ASP.NET.World-Literature/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace MVC.ASP.NET.World_Literature.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MVC.ASP.NET.World_Literature.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MVC.ASP.NET.World_Literature.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
